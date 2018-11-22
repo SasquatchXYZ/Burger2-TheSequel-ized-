b02ebe848adbd3369d6810a4ceb93419ab93b93c
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-21 21:14:34 -0500

    WIP

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,6 +12,8 @@ const db = require('../models/');
 router.get('/', function (req, res) {
     db.Burgers.findAll({}).then(function(data) {
         console.log(data);
+        const hbsObject = {Burgers: data};
+        res.render('index', hbsObject)
     });
 
     /*burger.selectAll(function (data) {
@@ -25,25 +27,42 @@ router.get('/', function (req, res) {
 
 // POST Route to add a new burger to the database and page.
 router.post('/api/burgers', function (req, res) {
-    console.log(req.body);
-    burger.insertOne([req.body.burger_name], function (result) {
+    //console.log(req.body);
+
+    db.Burgers.create({
+        burger_name: req.body.burger_name,
+        devoured: req.body.devoured
+    }).then(function(dbBurgers) {
+        res.json(dbBurgers)
+    });
+
+    /*burger.insertOne([req.body.burger_name], function (result) {
         res.status(200)
             .json({id: result.insertId});
-    });
+    });*/
 });
 
 // PUT Route to 'EAT' the burger, it changes the burgers status in the database and moves it on the page.
 router.put('/api/burgers/:id', function (req, res) {
-    const name = `id = ${req.params.id}`;
-    console.log('name', name);
+    console.log(req.params.id);
 
-    burger.updateOne(name, function (result) {
+    /*db.Burgers.update({
+        devoured: true
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(function(dbBurgers){
+        res.json(dbBurgers)
+    });*/
+
+    /*burger.updateOne(name, function (result) {
         if (result.changedRows === 0) {
             return res.status(404).end()
         } else {
             res.status(200).end()
         }
-    });
+    });*/
 });
 
 // Export routes for server.js to use.
